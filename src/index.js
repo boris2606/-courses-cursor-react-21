6e497c279bdbaf0edcd637e787e5a6aeefcbb6d8
Boris Shagiev <boris260692@gmail.com>
2023-02-21 14:44:19 +0200

    Fix Sign in

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="netlify.app">
+    <BrowserRouter basename="app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
